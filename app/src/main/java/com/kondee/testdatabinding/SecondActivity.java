f0a59a4b424dee77b17315e7f8748b18b42d0cdc
last3oy <softwarez_gelion@hotmail.com>
2018-10-15 23:52:47 +0700

    clean up code

--- app/src/main/java/com/kondee/testdatabinding/SecondActivity.java
+++ app/src/main/java/com/kondee/testdatabinding/SecondActivity.java
@@ -1,19 +1,20 @@
 package com.kondee.testdatabinding;
 
 import android.databinding.DataBindingUtil;
-import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.kondee.testdatabinding.databinding.ActivitySecondBinding;
+
 public class SecondActivity extends AppCompatActivity {
 
-//    private ActivitySecondBinding mBinding;
+    private ActivitySecondBinding mBinding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_second);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_second);
 
         initInstance();
     }
